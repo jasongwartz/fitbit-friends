a5970ebebaad8aa483560c6579b2a52f9c59b044
Jason Gwartz <jason.gwartz@ecosia.org>
2020-04-12 00:05:26 +0100

    add apply for master

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -7,9 +7,26 @@ data "aws_iam_policy" "dynamodb_full_access" {
   arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
 
+data "aws_iam_policy" "s3_full_access" {
+  arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+data "aws_iam_policy" "iam_read_only" {
+  arn = "arn:aws:iam::aws:policy/IAMReadOnlyAccess"
+}
+
 
 resource "aws_iam_user_policy_attachment" "github_ci_dynamodb" {
   user       = aws_iam_user.github_actions_ci.name
   policy_arn = data.aws_iam_policy.dynamodb_full_access.arn
 }
 
+resource "aws_iam_user_policy_attachment" "github_ci_s3" {
+  user       = aws_iam_user.github_actions_ci.name
+  policy_arn = data.aws_iam_policy.s3_full_access.arn
+}
+
+resource "aws_iam_user_policy_attachment" "github_ci_iam" {
+  user       = aws_iam_user.github_actions_ci.name
+  policy_arn = data.aws_iam_policy.iam_read_only.arn
+}
